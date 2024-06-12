c13da77ed73c22282b3b712b0713967e6d5ba245
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-06-12 09:27:27 +0200

    feat: create getProductsList lambda

--- product-service/lib/product-service-stack.ts
+++ product-service/lib/product-service-stack.ts
@@ -1,16 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
+import { aws_lambda } from "aws-cdk-lib";
+import { NodejsFunction, NodejsFunctionProps } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const commonProps: Partial<NodejsFunctionProps> =  {
+      runtime: aws_lambda.Runtime.NODEJS_20_X,
+      environment: {
+        PRODUCT_AWS_REGION: process.env.PRODUCT_AWS_REGION!,
+      }
+    }
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ProductServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const getProductsList = new NodejsFunction(this, 'GetProductsListLambda', {
+      ...commonProps,
+      entry: 'handlers/getProductsList.ts',
+      functionName: 'getProductsList',
+    });
   }
+
+
 }
